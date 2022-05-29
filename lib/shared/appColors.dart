e8b2f6534bd0ef7fb79a8630d599ee0ece00adf3
Amer Bešo <abeso1@etf.unsa.ba>
2022-05-29 19:35:51 +0200

    added splash view

---
+++ lib/shared/appColors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color mainGreenColor = const Color.fromRGBO(43, 222, 168, 1);
+}
---
+++ lib/views/views/splashView/splashView.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_show_app/shared/sizeConfig.dart';
+
+class SplashView extends StatefulWidget {
+  const SplashView({Key? key}) : super(key: key);
+
+  @override
+  _SplashViewState createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: ClipRRect(
+          borderRadius:
+              BorderRadius.circular(SizeConfig.blockSizeHorizontal * 4),
+          child: Image.asset(
+            "assets/images/logo.png",
+            width: SizeConfig.blockSizeHorizontal * 45,
+          ),
+        ),
+      ),
+    );
+  }
+}
